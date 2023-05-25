3f8b23f6e235bf3f4bf085b2284604dba8cbe001
SidiBecker <sidibecker@hotmail.com>
2023-05-24 21:38:51 -0300

    Adicionar arquivos de projeto.

---
+++ TaskManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserModel>> GetAll()
+        {
+
+            UserModel userModel = new UserModel();
+            userModel.Id = 1;
+            userModel.Name = "Test";
+            userModel.Email = "email@test.com";
+
+            List<UserModel> users = new List<UserModel>();
+            users.Add(userModel);
+
+            return Ok(users);
+        }
+    }
+}
